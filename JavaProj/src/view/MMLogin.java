34dc5de24a54fe641d914c26b576e98b474f69c1
SM013 <SM013@DESKTOP-24KD9VG>
2020-01-13 14:18:37 +0900

    book 커밋!!

--- JavaProj/src/view/MMLogin.java
+++ JavaProj/src/view/MMLogin.java
@@ -36,7 +36,7 @@ public class MMLogin {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 513, 360);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
