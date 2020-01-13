b2e03f1832ca638c702dd485588d6923f2ec5ac5
SM013 <SM013@DESKTOP-24KD9VG>
2020-01-13 14:06:40 +0900

    Member 커밋!!

---
+++ JavaProj/src/model/Member.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Member {
+	private String id;
+	private String pw;
+	private String name;
+	private String socialNumber;
+	private String address;
+	private String phone;
+	
+	public Member(String id, String pw, String name, String socialNumber, String address, String phone) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+		this.socialNumber = socialNumber;
+		this.address = address;
+		this.phone = phone;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSocialNumber() {
+		return socialNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+	
+	
+	
+	
+	
+}
