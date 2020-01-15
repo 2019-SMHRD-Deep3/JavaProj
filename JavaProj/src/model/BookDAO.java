8e66959b9f820e91ed1ff5e14ca937bf934ee66a
SM013 <SM013@DESKTOP-24KD9VG>
2020-01-15 16:57:04 +0900

    북 디에이오 수정

--- JavaProj/src/model/BookDAO.java
+++ JavaProj/src/model/BookDAO.java
@@ -13,23 +13,19 @@ public class BookDAO {
 	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
 	private String user = "hr";
 	private String password = "hr";
-	private Connection conn = null; // connection import필요
+	private Connection conn = null;
 	private PreparedStatement psmt = null;
 	private ResultSet rs = null;
 
 	public int insert(Book b) {
 		int rows = 0;
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver"); /// try - 예외색출 //catch - 예외처리
-			/// surround with try/catch//java에서 예외처리-문법외의 에러, 실행중에 발행하는 에러
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, user, password);
 
-			conn = DriverManager.getConnection(url, user, password); // 접속시도 , 코딩후 에러 - 캐치블럭 설정(팝업창2번째)
-			/// connection - overloading
-			// String sql = "insert into member values('kkb2','김김김',25,1)" ;
 			String sql = "insert into Book values(?,?,?,?,?,?)";
-			//// 정보를 입력받아서 실행시키는 방법 // 상기 문장 대체 //하단 psmt문 활용
 
-			psmt = conn.prepareStatement(sql); // 지역변수 생성
+			psmt = conn.prepareStatement(sql);
 
 			psmt.setString(1, b.getTitle());
 			psmt.setString(2, b.getIsbn());
@@ -38,35 +34,30 @@ public class BookDAO {
 			psmt.setString(5, b.getLoanDate());
 			psmt.setString(6, b.getReturnDate());
 
-			rows = psmt.executeUpdate(); /// psmt 준비시 실행 cf select시에는 executeQuery();
-//				if(rows == 0) { //rows가 0인 경우는 문제가 있다는 의미
-//					System.out.println("SQL문을 확인하세요.");
-//				}
-
+			rows = psmt.executeUpdate();
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
-		} // 클래스 객체를 생성해서 메모리에 올려주는 역할
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (SQLException e) {
+
 			e.printStackTrace();
 		} finally {
-			/// 접속 out은 접속 in의 역순으로
+
 			try {
 				if (psmt != null)
 					psmt.close();
 
 			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
+
 				e1.printStackTrace();
 			}
 			try {
 				if (conn != null)
 					conn.close();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
-			} // error에러 --> 처리 ; surround with try/catch
+			}
 
 		}
 
@@ -77,12 +68,10 @@ public class BookDAO {
 
 		Book book = null;
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver"); /// try - 예외색출 //catch - 예외처리
-			/// surround with try/catch//java에서 예외처리-문법외의 에러, 실행중에 발행하는 에러
+			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			conn = DriverManager.getConnection(url, user, password); // 접속시도 , 코딩후 에러 - 캐치블럭 설정(팝업창2번째)
-			/// connection - overloading
-			// String sql = "insert into member values('kkb2','김김김',25,1)" ;
+			conn = DriverManager.getConnection(url, user, password);
+
 			String sql = "SELECT * FROM Book " + "WHERE ISBM = ? TITLE=?";
 			psmt = conn.prepareStatement(sql);
 			psmt.setString(1, b.getIsbn());
@@ -90,8 +79,8 @@ public class BookDAO {
 
 			rs = psmt.executeQuery();
 
-			if (rs.next()) { // 다음 줄 유무를 true, false로 전달
-				// 해당 ID와 PW를 가진 사람이 존재 //하단 데이터베이스에 있는 컬럼명
+			if (rs.next()) {
+
 				String title = rs.getString("title");
 				String isbn = rs.getString("isbn");
 				String author = rs.getString("author");
@@ -102,20 +91,15 @@ public class BookDAO {
 				b = new Book(title, isbn, author, publisher);
 
 			}
-			//// 정보를 입력받아서 실행시키는 방법 // 상기 문장 대체 //하단 psmt문 활용
-//				if(rows == 0) { //rows가 0인 경우는 문제가 있다는 의미
-//					System.out.println("SQL문을 확인하세요.");
-//				}
 
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
-		} // 클래스 객체를 생성해서 메모리에 올려주는 역할
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (SQLException e) {
+
 			e.printStackTrace();
 		} finally {
-			/// 접속 out은 접속 in의 역순으로
+
 			try {
 				if (rs != null)
 					rs.close();
@@ -123,16 +107,16 @@ public class BookDAO {
 					psmt.close();
 
 			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
+
 				e1.printStackTrace();
 			}
 			try {
 				if (conn != null)
 					conn.close();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
-			} // error에러 --> 처리 ; surround with try/catch
+			}
 
 		}
 		return book;
@@ -143,12 +127,10 @@ public class BookDAO {
 		ArrayList<Book> list = new ArrayList<>();
 
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver"); /// try - 예외색출 //catch - 예외처리
-			/// surround with try/catch//java에서 예외처리-문법외의 에러, 실행중에 발행하는 에러
+			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			conn = DriverManager.getConnection(url, user, password); // 접속시도 , 코딩후 에러 - 캐치블럭 설정(팝업창2번째)
-			/// connection - overloading
-			// String sql = "insert into book values(?,?,?,?,?,?)" ;
+			conn = DriverManager.getConnection(url, user, password);
+
 			String sql = "SELECT * FROM MEMBER " + "WHERE TITLE != ? ";
 			psmt = conn.prepareStatement(sql);
 
@@ -166,20 +148,15 @@ public class BookDAO {
 				list.add(new Book(title2, isbn, author, publisher));
 
 			}
-			//// 정보를 입력받아서 실행시키는 방법 // 상기 문장 대체 //하단 psmt문 활용
-//		if(rows == 0) { //rows가 0인 경우는 문제가 있다는 의미
-//			System.out.println("SQL문을 확인하세요.");
-//		}
 
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
-		} // 클래스 객체를 생성해서 메모리에 올려주는 역할
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (SQLException e) {
+
 			e.printStackTrace();
 		} finally {
-			/// 접속 out은 접속 in의 역순으로
+
 			try {
 				if (rs != null)
 					rs.close();
@@ -187,16 +164,15 @@ public class BookDAO {
 					psmt.close();
 
 			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 			try {
 				if (conn != null)
 					conn.close();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
-			} // error에러 --> 처리 ; surround with try/catch
+			}
 
 		}
 		return list;
@@ -228,14 +204,13 @@ public class BookDAO {
 			}
 
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
-		} // 클래스 객체를 생성해서 메모리에 올려주는 역할
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (SQLException e) {
+
 			e.printStackTrace();
 		} finally {
-			/// 접속 out은 접속 in의 역순으로
+
 			try {
 				if (rs != null)
 					rs.close();
@@ -243,56 +218,39 @@ public class BookDAO {
 					psmt.close();
 
 			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
+
 				e1.printStackTrace();
 			}
 			try {
 				if (conn != null)
 					conn.close();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
-			} // error에러 --> 처리 ; surround with try/catch
+			}
 
 		}
 		return list;
 
 	}
 
-	private static void update(Book b) { // 매개변수들을 하나의 객체(class)로 간주하고 처리하는 방법은 exam05 참조
-		// JDBC 활용순서
-		// 1. 사용할 데이터베이스 회사의 jar파일을 프로젝트에 import (process 별첨)
-		// 2. 접속을 위한 프로젝트 파일 로딩
-		// 3. 데이터베이스에 접속 --> 아이디, 비밀번호, 데이터베이스 주소
-		// 4. SQL 작성을 돕는 객체 생성
-		// 5. SQL 실행
-		// 6. Select의 경우에 객체로부터 데이터 추출
-		// 7. 접속종료
+	private static void update(Book b) {
 
-		String url = "jdbc:oracle:thin:@localhost:1521:xe"; /// 오라클사에서 정해놓은 버전정보 ///localhost; 현재 내컴퓨터를 지칭/// 1521 //xe
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String user = "hr";
 		String password = "hr";
-		Connection conn = null; // connection import필요
+		Connection conn = null;
 		PreparedStatement psmt = null;
 
-		//// 2. 접속을 위한 프로젝트 파일 로딩
-
-		/// OracleDriver driver = new OracleDriver(); 수정시 직접 compile 수행요, 번거로움
-		//// String name = 설정파일에서 드라이버명 읽기(); 이 경우 - 수정시 메모장에 있는 글자만 변경, recompile불요
-
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver"); /// try - 예외색출 //catch - 예외처리
-			/// surround with try/catch//java에서 예외처리-문법외의 에러, 실행중에 발행하는 에러
+			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			conn = DriverManager.getConnection(url, user, password); // 접속시도 , 코딩후 에러 - 캐치블럭 설정(팝업창2번째)
-			/// connection - overloading
-			// String sql = "insert into member values('kkb2','김김김',25,1)" ;
+			conn = DriverManager.getConnection(url, user, password);
+
 			String sql = "update book set title = ?, isbn = ?, author = ?, publisher = ?, "
 					+ "loanDate = ?, returnDate = ? WHERE USER_id = ?";
-			// String sql = "update member set USER_AGE = ? WHERE USER_ID = ?";
-			//// 정보를 입력받아서 실행시키는 방법 // 상기 문장 대체 //하단 psmt문 활용
 
-			psmt = conn.prepareStatement(sql); // 지역변수 생성
+			psmt = conn.prepareStatement(sql);
 
 			psmt.setString(1, b.getTitle());
 			psmt.setString(2, b.getIsbn());
@@ -308,20 +266,19 @@ public class BookDAO {
 			}
 
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
-		} // 클래스 객체를 생성해서 메모리에 올려주는 역할
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (SQLException e) {
+
 			e.printStackTrace();
 		} finally {
-			/// 접속 out은 접속 in의 역순으로
+
 			try {
 				if (psmt != null)
 					psmt.close();
 
 			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
+
 				e1.printStackTrace();
 			}
 			try {
@@ -329,69 +286,44 @@ public class BookDAO {
 					conn.close();
 
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
-			} // error에러 --> 처리 ; surround with try/catch
+			}
 
 		}
 	}
 
-	//// 4. SQL 작성을 돕는 객체 생성
-
-	//////// 2. conn 하단 코딩 참조
-
-	//// 5. 실행
-
-	//// 7. 접속종료 //접속 종료를 수행해야 connection객체도 out db에서 commit 수행후 java에서 실행
-
 	private static String getReturnDate() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	private static String getLoanDate() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	private static String getPublisher() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
-	private static void delete(Book b) { // 매개변수들을 하나의 객체(class)로 간주하고 처리하는 방법은 exam05 참조
-		// JDBC 활용순서
-		// 1. 사용할 데이터베이스 회사의 jar파일을 프로젝트에 import (process 별첨)
-		// 2. 접속을 위한 프로젝트 파일 로딩
-		// 3. 데이터베이스에 접속 --> 아이디, 비밀번호, 데이터베이스 주소
-		// 4. SQL 작성을 돕는 객체 생성
-		// 5. SQL 실행
-		// 6. Select의 경우에 객체로부터 데이터 추출
-		// 7. 접속종료
+	private static void delete(Book b) {
 
-		String url = "jdbc:oracle:thin:@localhost:1521:xe"; /// 오라클사에서 정해놓은 버전정보 ///localhost; 현재 내컴퓨터를 지칭/// 1521 //xe
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String user = "hr";
 		String password = "hr";
-		Connection conn = null; // connection import필요
+		Connection conn = null;
 		PreparedStatement psmt = null;
 
-		//// 2. 접속을 위한 프로젝트 파일 로딩
-
-		/// OracleDriver driver = new OracleDriver(); 수정시 직접 compile 수행요, 번거로움
-		//// String name = 설정파일에서 드라이버명 읽기(); 이 경우 - 수정시 메모장에 있는 글자만 변경, recompile불요
-
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver"); /// try - 예외색출 //catch - 예외처리
-			/// surround with try/catch//java에서 예외처리-문법외의 에러, 실행중에 발행하는 에러
+			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			conn = DriverManager.getConnection(url, user, password); // 접속시도 , 코딩후 에러 - 캐치블럭 설정(팝업창2번째)
-			/// connection - overloading
-			// String sql = "insert into member values('kkb2','김김김',25,1)" ;
+			conn = DriverManager.getConnection(url, user, password);
+
 			String sql = "delete from  users WHERE USER_id = ?";
-			// String sql = "delete from users WHERE USER_ID = ?";
-			//// 정보를 입력받아서 실행시키는 방법 // 상기 문장 대체 //하단 psmt문 활용
 
-			psmt = conn.prepareStatement(sql); // 지역변수 생성
+			psmt = conn.prepareStatement(sql);
 
 			psmt.setString(1, b.getTitle());
 			psmt.setString(2, b.getIsbn());
@@ -407,20 +339,16 @@ public class BookDAO {
 			}
 
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
-		} // 클래스 객체를 생성해서 메모리에 올려주는 역할
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			/// 접속 out은 접속 in의 역순으로
 			try {
 				if (psmt != null)
 					psmt.close();
 
 			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 			try {
@@ -428,19 +356,10 @@ public class BookDAO {
 					conn.close();
 
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} // error에러 --> 처리 ; surround with try/catch
+			}
 
 		}
 	}
-
-	//// 4. SQL 작성을 돕는 객체 생성
-
-	//////// 2. conn 하단 코딩 참조
-
-	//// 5. 실행
-
-	//// 7. 접속종료 //접속 종료를 수행해야 connection객체도 out db에서 commit 수행후 java에서 실행
 
 }
--- JavaProj/src/view/D.java
+++ JavaProj/src/view/D.java
@@ -1,7 +1,8 @@
 package view;
 
 import java.awt.Color;
-import java.awt.Frame;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -10,118 +11,107 @@ import java.util.ArrayList;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.SwingConstants;
 
 import controller.MemberManagementService;
 import model.Book;
 import model.Member;
-import java.awt.Font;
 
-public class D extends JPanel {	// ÀÌ´ÞÀÇ ÃßÃµµµ¼­ È¤Àº Åë°è
+public class D extends JPanel { // ÀÌ´ÞÀÇ ÃßÃµµµ¼­ È¤Àº Åë°è
 	private MemberManagementService service = new MemberManagementService();
 	private Member loginUser;
+	private JPanel panel;
+	private JFrame frame;
+
 	/**
 	 * Create the panel.
 	 */
 	public D() {
 		setBackground(Color.LIGHT_GRAY);
 		setLayout(null);
-		
-		String[] combo = {"(¼±ÅÃÇÒ ¿¬µµ ¹× ¿ù)", "2020³â 1¿ù", "2020³â 2¿ù"};
+
+		String[] combo = { "(¼±ÅÃÇÒ ¿¬µµ ¹× ¿ù)", "2020³â 1¿ù", "2020³â 2¿ù" };
 		DefaultComboBoxModel<String> comboModel = new DefaultComboBoxModel<String>(combo);
-	    JComboBox comboBox = new JComboBox(comboModel);
+		JComboBox comboBox = new JComboBox(comboModel);
 		comboBox.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				
+
 			}
 		});
 		comboBox.setBounds(150, 57, 200, 31);
 		add(comboBox);
-		
-		String imgPath = this.getClass().getResource(".").getPath() + "..//..//img//btn002.png";
-		ImageIcon icon = new ImageIcon();
-		System.out.println(imgPath);
 
-		JPanel panel = new JPanel() {
-			@Override
-			protected void paintComponent(Graphics g) {
-				g.drawImage(icon.getImage(), 150, 125, 300, 400, null);
-				setOpaque(false);
-				super.paintComponent(g);
-			}
-		};
-		
-		
+		JPanel panel = new JPanel();
+		panel.setBounds(150, 125, 300, 400);
+		add(panel);
 
-		
-		
+
 		JLabel lblNewLabel = new JLabel("\uB3C4\uC11C\uBA85");
 		lblNewLabel.setFont(new Font("¸¼Àº °íµñ Semilight", Font.PLAIN, 15));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(500, 150, 100, 31);
 		add(lblNewLabel);
-		
+
 		JLabel label = new JLabel("\uC800  \uC790");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setFont(new Font("¸¼Àº °íµñ Semilight", Font.PLAIN, 15));
 		label.setBounds(500, 200, 100, 31);
 		add(label);
-		
+
 		JLabel label_1 = new JLabel("\uCD9C\uD310\uC0AC");
 		label_1.setHorizontalAlignment(SwingConstants.CENTER);
 		label_1.setFont(new Font("¸¼Àº °íµñ Semilight", Font.PLAIN, 15));
 		label_1.setBounds(500, 250, 100, 31);
 		add(label_1);
-		
+
 		JLabel label_2 = new JLabel("\uC7A5  \uB974");
 		label_2.setHorizontalAlignment(SwingConstants.CENTER);
 		label_2.setFont(new Font("¸¼Àº °íµñ Semilight", Font.PLAIN, 15));
 		label_2.setBounds(500, 300, 100, 31);
 		add(label_2);
-		
+
 		JLabel label_3 = new JLabel("ISBN");
 		label_3.setHorizontalAlignment(SwingConstants.CENTER);
 		label_3.setFont(new Font("¸¼Àº °íµñ Semilight", Font.PLAIN, 15));
 		label_3.setBounds(500, 350, 100, 31);
 		add(label_3);
-		
+
 		JLabel bookTitle = new JLabel("");
 		bookTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		bookTitle.setFont(new Font("¸¼Àº °íµñ Semilight", Font.PLAIN, 15));
 		bookTitle.setBounds(600, 150, 200, 31);
-//		panel.getContentPane().add(name);
-//		name.setText(loginUser.getName());
+		frame.getContentPane().add(bookTitle);
+		bookTitle.setText(loginUser.getName());
 		add(bookTitle);
-		
+
 		JLabel bookAuthor = new JLabel("");
 		bookAuthor.setHorizontalAlignment(SwingConstants.CENTER);
 		bookAuthor.setFont(new Font("¸¼Àº °íµñ Semilight", Font.PLAIN, 15));
 		bookAuthor.setBounds(600, 200, 200, 31);
 		add(bookAuthor);
-		
+
 		JLabel bookPublisher = new JLabel("");
 		bookPublisher.setHorizontalAlignment(SwingConstants.CENTER);
 		bookPublisher.setFont(new Font("¸¼Àº °íµñ Semilight", Font.PLAIN, 15));
 		bookPublisher.setBounds(600, 250, 200, 31);
 		add(bookPublisher);
-		
+
 		JLabel bookGenre = new JLabel("");
 		bookGenre.setHorizontalAlignment(SwingConstants.CENTER);
 		bookGenre.setFont(new Font("¸¼Àº °íµñ Semilight", Font.PLAIN, 15));
 		bookGenre.setBounds(600, 300, 200, 31);
 		add(bookGenre);
-		
+
 		JLabel bookIsbn = new JLabel("");
 		bookIsbn.setHorizontalAlignment(SwingConstants.CENTER);
 		bookIsbn.setFont(new Font("¸¼Àº °íµñ Semilight", Font.PLAIN, 15));
 		bookIsbn.setBounds(600, 350, 200, 31);
 		add(bookIsbn);
-		
+
 		String[] columnNames = { "Ã¥ÀÌ¸§", "ÀúÀÚ", "ÃâÆÇ»ç", "ISBN" };
 
 		ArrayList<Book> list = service.bookLookup();
