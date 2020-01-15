ba3c83fdffc35e9f59a10d043e69b84fdf029e5c
SM013 <SM013@DESKTOP-24KD9VG>
2020-01-15 15:08:11 +0900

    유저 화면 생성

---
+++ JavaProj/src/view/BU.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class BU extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public BU() {
+
+	}
+
+}
---
+++ JavaProj/src/view/CU.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class CU extends JPanel {	// 자신의 연체 대출 정보만 보고, 1회 추가 연장(+2주) 가능
+
+	/**
+	 * Create the panel.
+	 */
+	public CU() {
+
+	}
+
+}
