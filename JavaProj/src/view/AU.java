e655a47fac082d7d63f02293a506983fdcc655a6
SM013 <SM013@DESKTOP-24KD9VG>
2020-01-15 15:08:21 +0900

    유저 화면 등록

--- JavaProj/src/view/AU.java
+++ JavaProj/src/view/AU.java
@@ -2,7 +2,7 @@ package view;
 
 import javax.swing.JPanel;
 
-public class AU extends JPanel {
+public class AU extends JPanel {	// 회원정보 중 자신의 데이터 확인만 가능 (아이디, 패스워드, 이름, 주민번호, 주소, 연락처)
 
 	/**
 	 * Create the panel.
--- JavaProj/src/view/BU.java
+++ JavaProj/src/view/BU.java
@@ -2,7 +2,7 @@ package view;
 
 import javax.swing.JPanel;
 
-public class BU extends JPanel {
+public class BU extends JPanel {	// 도서 목록 검색만 가능(추가, 삭제, 수정 등 불가)
 
 	/**
 	 * Create the panel.
