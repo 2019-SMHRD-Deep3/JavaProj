50bcb208c7af6936efc23c2f3e9768f201df0fed
SM013 <SM013@DESKTOP-24KD9VG>
2020-01-16 16:53:22 +0900

    if문 주석처리

--- JavaProj/src/view/MMMain.java
+++ JavaProj/src/view/MMMain.java
@@ -45,7 +45,7 @@ public class MMMain {
 		cardLayout = new CardLayout(0, 0);
 		cardPanel.setLayout(cardLayout);
 
-		if (loginUser.getId().equals("admin")) {
+//		if (loginUser.getId().equals("admin")) {
 			DFirst d1Panel = new DFirst();
 			cardPanel.add(d1Panel, "d1");
 
@@ -60,22 +60,22 @@ public class MMMain {
 
 			D dPanel = new D();
 			cardPanel.add(dPanel, "d");
-		} else {
-			DFirst d1Panel = new DFirst();
-			cardPanel.add(d1Panel, "d1");
+//		} else {
+//			DFirst d1Panel = new DFirst();
+//			cardPanel.add(d1Panel, "d1");
 
 			AU auPanel = new AU(loginUser);
 			cardPanel.add(auPanel, "au");
 
 			BU buPanel = new BU();
 			cardPanel.add(buPanel, "bu");
-
+ 
 			CU cuPanel = new CU();
 			cardPanel.add(cuPanel, "cu");
 
-			D dPanel = new D();
-			cardPanel.add(dPanel, "d");
-		}
+//			D dPanel = new D();
+//			cardPanel.add(dPanel, "d");
+//		}
 
 		JLabel id = new JLabel("ooo");
 		id.setFont(new Font("¸¼Àº °íµñ Semilight", Font.PLAIN, 15));
