b22ec3d5de1f05e1887b0f6c59c9e2db077f44ed
SM013 <SM013@DESKTOP-24KD9VG>
2020-01-20 09:13:25 +0900

    마무리

--- JavaProj/src/model/BookStDAO.java
+++ JavaProj/src/model/BookStDAO.java
@@ -13,66 +13,53 @@ import java.util.ArrayList;
 
 public class BookStDAO {
 
-	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
-	private String user = "hr";
-	private String password = "hr";
-	private Connection conn = null;
-	private PreparedStatement psmt = null;
-	private ResultSet rs = null;
-	private Book b;
+   private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+   private String user = "hr";
+   private String password = "hr";
+   private Connection conn = null;
+   private PreparedStatement psmt = null;
+   private ResultSet rs = null;
+   private Book b;
 
-	public ArrayList<Book> rankFive(String login_id) {
+   public ArrayList<Book> rankFive(String login_id) {
 
-		ArrayList<Book> list = new ArrayList<>();
+      ArrayList<Book> list = new ArrayList<>();
 
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection(url, user, password);
-			String sql = "SELECT b_title, b_count, ROW_NUMBER() OVER (ORDER BY b_count DESC) FROM Book WHERE b_isbn = ? AND ROWNUM <= 5";
-			psmt = conn.prepareStatement(sql);
+      try {
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+         conn = DriverManager.getConnection(url, user, password);
+         String sql = "SELECT b.b_title, l.l_count, ROW_NUMBER() OVER (ORDER BY l.l_count DESC) FROM loan l, book b WHERE b.b_isbn = l.b_isbn AND ROWNUM <= 5";
+         ;
+         psmt = conn.prepareStatement(sql);
 
-			File file2 = new File("D:\\BookIsbn.txt");
-			FileReader file_reader2 = new FileReader(file2);
-			int cur2 = 0;
-			String strBookISBN = "";
-			while ((cur2 = file_reader2.read()) != -1) {
-				strBookISBN += (char) cur2;
-			}
-			psmt.setLong(1, Long.parseLong(strBookISBN));
+         rs = psmt.executeQuery();
 
-			rs = psmt.executeQuery();
+         while (rs.next()) {
+            String title = rs.getString("B_TITLE");
+            int cnt = rs.getInt("L_COUNT");
+            list.add(new Book(title, cnt));
+         }
+      } catch (ClassNotFoundException e) {
+         e.printStackTrace();
+      } catch (SQLException e) {
+         e.printStackTrace();
 
-			while (rs.next()) {
-				String title = rs.getString("B_TITLE");
-				int cnt = rs.getInt("b_COUNT");
-				list.add(new Book(title, cnt));
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			try {
-				if (rs != null)
-					rs.close();
-				if (psmt != null)
-					psmt.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			try {
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return list;
-	}
+      } finally {
+         try {
+            if (rs != null)
+               rs.close();
+            if (psmt != null)
+               psmt.close();
+         } catch (SQLException e) {
+            e.printStackTrace();
+         }
+         try {
+            if (conn != null)
+               conn.close();
+         } catch (SQLException e) {
+            e.printStackTrace();
+         }
+      }
+      return list;
+   }
 }
--- JavaProj/src/view/AU.java
+++ JavaProj/src/view/AU.java
@@ -18,84 +18,84 @@ public class AU extends JPanel {	// »∏ø¯¡§∫∏ ¡ﬂ ¿⁄Ω≈¿« µ•¿Ã≈Õ »Æ¿Œ∏∏ ∞°¥… (æ∆¿Ãµ
 	 */
 	public AU(Member loginUser) {
 		this.loginUser = loginUser;
-		setBackground(Color.WHITE);
+		setBackground(new Color(240, 245, 249));
 		setLayout(null);
 		
 		JLabel lblId = new JLabel("ID");
 		lblId.setHorizontalAlignment(SwingConstants.CENTER);
 		lblId.setFont(new Font("∏º¿∫ ∞ÌµÒ Semilight", Font.PLAIN, 15));
-		lblId.setBounds(75, 75, 100, 31);
+		lblId.setBounds(279, 155, 100, 31);
 		add(lblId);
 		
 		JLabel id = new JLabel("");
 		id.setHorizontalAlignment(SwingConstants.CENTER);
 		id.setFont(new Font("∏º¿∫ ∞ÌµÒ Semilight", Font.PLAIN, 15));
-		id.setBounds(175, 75, 200, 31);
+		id.setBounds(379, 155, 200, 31);
 		add(id);
 		id.setText(loginUser.getId());
 		
 		JLabel lblPw = new JLabel("\uC804\uD654\uBC88\uD638");
 		lblPw.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPw.setFont(new Font("∏º¿∫ ∞ÌµÒ Semilight", Font.PLAIN, 15));
-		lblPw.setBounds(75, 125, 100, 31);
+		lblPw.setBounds(279, 205, 100, 31);
 		add(lblPw);
 		
 		JLabel pw = new JLabel("");
 		pw.setHorizontalAlignment(SwingConstants.CENTER);
 		pw.setFont(new Font("∏º¿∫ ∞ÌµÒ Semilight", Font.PLAIN, 15));
-		pw.setBounds(175, 125, 200, 31);
+		pw.setBounds(379, 205, 200, 31);
 		add(pw);
 		pw.setText(loginUser.getPw());
 		
 		JLabel label_4 = new JLabel("\uBE44\uBC00\uBC88\uD638");
 		label_4.setHorizontalAlignment(SwingConstants.CENTER);
 		label_4.setFont(new Font("∏º¿∫ ∞ÌµÒ Semilight", Font.PLAIN, 15));
-		label_4.setBounds(75, 175, 100, 31);
+		label_4.setBounds(279, 255, 100, 31);
 		add(label_4);
 		
 		JLabel name = new JLabel("");
 		name.setHorizontalAlignment(SwingConstants.CENTER);
 		name.setFont(new Font("∏º¿∫ ∞ÌµÒ Semilight", Font.PLAIN, 15));
-		name.setBounds(175, 175, 200, 31);
+		name.setBounds(379, 255, 200, 31);
 		add(name);
 		name.setText(loginUser.getName());
 		
 		JLabel label_6 = new JLabel("\uC774\uB984");
 		label_6.setHorizontalAlignment(SwingConstants.CENTER);
 		label_6.setFont(new Font("∏º¿∫ ∞ÌµÒ Semilight", Font.PLAIN, 15));
-		label_6.setBounds(75, 225, 100, 31);
+		label_6.setBounds(279, 305, 100, 31);
 		add(label_6);
 		
 		JLabel socialNumber = new JLabel("");
 		socialNumber.setHorizontalAlignment(SwingConstants.CENTER);
 		socialNumber.setFont(new Font("∏º¿∫ ∞ÌµÒ Semilight", Font.PLAIN, 15));
-		socialNumber.setBounds(175, 225, 200, 31);
+		socialNumber.setBounds(379, 305, 200, 31);
 		add(socialNumber);
 		socialNumber.setText(loginUser.getSocialNumber());
 		
 		JLabel label_8 = new JLabel("\uC8FC\uC18C");
 		label_8.setHorizontalAlignment(SwingConstants.CENTER);
 		label_8.setFont(new Font("∏º¿∫ ∞ÌµÒ Semilight", Font.PLAIN, 15));
-		label_8.setBounds(75, 278, 100, 31);
+		label_8.setBounds(279, 358, 100, 31);
 		add(label_8);
 		
 		JLabel phoneNumber = new JLabel("");
 		phoneNumber.setHorizontalAlignment(SwingConstants.CENTER);
 		phoneNumber.setFont(new Font("∏º¿∫ ∞ÌµÒ Semilight", Font.PLAIN, 15));
-		phoneNumber.setBounds(175, 278, 200, 31);
+		phoneNumber.setBounds(379, 358, 200, 31);
 		add(phoneNumber);
 		phoneNumber.setText(loginUser.getPhone());
 		
 		JLabel label_10 = new JLabel("\uC8FC\uBBFC\uB4F1\uB85D\uBC88\uD638");
 		label_10.setHorizontalAlignment(SwingConstants.CENTER);
 		label_10.setFont(new Font("∏º¿∫ ∞ÌµÒ Semilight", Font.PLAIN, 15));
-		label_10.setBounds(75, 328, 100, 31);
+		label_10.setBounds(279, 408, 100, 31);
 		add(label_10);
 		
 		JLabel address = new JLabel("");
 		address.setHorizontalAlignment(SwingConstants.CENTER);
 		address.setFont(new Font("∏º¿∫ ∞ÌµÒ Semilight", Font.PLAIN, 15));
-		address.setBounds(175, 328, 200, 31);
+		address.setBounds(379, 408, 200, 31);
 		add(address);
 		address.setText(loginUser.getAddress());
 
